19e425c784e9f30636a9ab594e5d19d8afc08239
mhmmdo <e020323074@mahasiswa.polibal.ac.id>
2024-02-04 16:42:18 +0800

    update

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# Neo Python Bot WhatsApp
+
+# Use lib Neonize
+# https://github.com/krypton-byte/neonize
+
+# Thanks to :
+# https://github.com/krypton-byte
+
 import logging
 import os
 import signal
@@ -16,6 +24,7 @@ from neonize.events import (
     ReceiptEv,
     CallOfferEv,
 )
+
 from neonize.proto.def_pb2 import ImageMessage
 from neonize.types import MessageServerID
 from neonize.utils import log
@@ -56,29 +65,55 @@ def on_message(client: NewClient, message: MessageEv):
     handler(client, message)
 
 def handler(client: NewClient, message: MessageEv):
-	msg = message.Info # .__str__()
-    # text = msg.Message.conversation or message.Message.extendedTextMessage.text
-    from = msg.MessageSource.Chat
-    msg_message_keys = list(msg['message'].keys())
-    type = msg_message_keys[0]
-    chats = ""
-	if type == 'conversation' and 'conversation' in msg['Message']:
-	    chats = msg['message']['conversation']
-	elif type == 'imageMessage' and 'imageMessage' in msg['Message'] and 'caption' in msg['Message']['imageMessage']:
-	    chats = msg['Message']['imageMessage']['caption']
-	elif type == 'documentMessage' and 'documentMessage' in msg['Message'] and 'caption' in msg['Message']['documentMessage']:
-	    chats = msg['Message']['documentMessage']['caption']
-	elif type == 'videoMessage' and 'videoMessage' in msg['Message'] and 'caption' in msg['Message']['videoMessage']:
-	    chats = msg['Message']['videoMessage']['caption']
-	elif type == 'extendedTextMessage' and 'extendedTextMessage' in msg['Message'] and 'text' in msg['Message']['extendedTextMessage']:
-	    chats = msg['Message']['extendedTextMessage']['text']
-    isGroup = msg.isGroup
-    log.info(msg.Chat.User)
-    log.info(chats)
-    # log.info("")
-    match text:
-    	case "ping":
-    		client.send_message(from, "Pong")
+    text = message.Message.conversation or message.Message.extendedTextMessage.text
+    msg = message.Message  # Access the 'Message' attribute
+    chat = message.Info.MessageSource.Chat
+    prefix = "!"
+    # log.info(message__str__()) debug msg
+    # Check the type of the message
+    if msg.conversation:
+        chats = msg.conversation
+    elif msg.imageMessage and msg.imageMessage.caption:
+        chats = msg.imageMessage.caption
+    elif msg.documentMessage and msg.documentMessage.caption:
+        chats = msg.documentMessage.caption
+    elif msg.videoMessage and msg.videoMessage.caption:
+        chats = msg.videoMessage.caption
+    elif msg.extendedTextMessage and msg.extendedTextMessage.text:
+        chats = msg.extendedTextMessage.text
+    else:
+        chats = ""
+
+    if chats.startswith(prefix):
+        args = chats[len(prefix):].split(' ')
+        command = args[0].lower() if args else ''
+
+        # is_group = message.isGroup
+        log.info(message.__str__())
+        log.info(chats)
+        match command:
+            case "menu":
+                txt = "Neo Bot WhatsApp\n\n"
+                txt += "</ Command >\n"
+                txt += "!menu\n"
+                txt += "!ping\n"
+                txt += "!sticker < media img/vid/gif to sticker >"
+                client.send_message(chat, txt)
+            case "ping":
+                client.send_message(chat, "Pong")
+            case "sticker":
+                if message.Info.Type == "media":
+                    client.send_sticker(
+                        chat,
+                        client.download_any(message.Message),
+                        quoted=message,
+                        name="@Neo Bot WhatsApp",
+                        packname="@mhmmdoo_",
+                    )
+                else:
+                    client.reply_message("Tolong kirimkan media gambar/video/gif dengan caption !sticker", quoted=message)
+
+            # for more example visit "https://github.com/krypton-byte/neonize/blob/master/examples/basic.py"
 
 
 @client.event(PairStatusEv)
